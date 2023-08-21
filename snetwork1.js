df55c0ae84673aab0104395c8e74027e32a9383f
WizdomDepot <76233440+WizdomDepot@users.noreply.github.com>
2023-08-21 18:37:22 +0100

    Create snetwork.js

---
+++ snetwork1.js
@@ -0,0 +1,18 @@
+export const data = [
+  {
+    network: "Mtn",
+    value: 1
+  },
+  {
+    network: "Glo",
+    value: 2
+  },
+  {
+    network: "Airtel",
+    value: 3
+  },
+  {
+    network: "9mobile",
+    value: 4
+  }
+]
