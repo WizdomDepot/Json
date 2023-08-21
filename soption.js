f837269020c77615eccffe1f92e51d536f7e913f
WizdomDepot <76233440+WizdomDepot@users.noreply.github.com>
2023-08-21 18:27:23 +0100

    Create soption.js

---
+++ soption.js
@@ -0,0 +1,57 @@
+export const mtn = [
+    { value:1,
+    option: "500mb Mtn = 125 30 days" },
+    { value:2,
+    option: "1gb mtn = 250 30 days" },
+    { value:3,
+    option: "2gb mtn = 500 30 days" },
+    { value:3,
+    option: "3gb mtn = 750 30 days" },
+    { value:3,
+    option: "5gb mtn = 1250 30 days" },
+    { value:3,
+    option: "10gb mtn = 2500 30 days" }
+  ]
+ export const glo = [
+    { value:1,
+    option: "500mb glo = 125 30 days" },
+    { value:2,
+    option: "1gb glo = 250 30 days" },
+    { value:3,
+    option: "2gb glo = 500 30 days" },
+    { value:3,
+    option: "3gb glo = 750 30 days" },
+    { value:3,
+    option: "5gb glo = 1250 30 days" },
+    { value:3,
+    option: "10gb glo = 2500 30 days" }
+  ]
+export const Airtel = [
+    { value:1,
+    option: "500mb Airtel = 125 30 days" },
+    { value:2,
+    option: "1gb Airtel = 250 30 days" },
+    { value:3,
+    option: "2gb Airtel = 500 30 days" },
+    { value:3,
+    option: "3gb Airtel = 750 30 days" },
+    { value:3,
+    option: "5gb Airtel = 1250 30 days" },
+    { value:3,
+    option: "10gb Airtel = 2500 30 days" }
+  ]
+ export const nmobile  = [
+    { value:1,
+    option: "500mb 9mobile = 100 30 days" },
+    { value:2,
+    option: "1gb 9mobile = 200 30 days" },
+    { value:3,
+    option: "2gb 9mobile = 400 30 days" },
+    { value:3,
+    option: "3gb mtn = 600 30 days" },
+    { value:3,
+    option: "5gb mtn = 1000 30 days" },
+    { value:3,
+    option: "10gb mtn = 2000 30 days" }
+  ]
+  
